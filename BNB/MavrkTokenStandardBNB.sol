8203b52814c19a77971ce02c058c8ab24029b265
mavrkofficial <info@mavrk.xyz>
2025-05-05 15:49:45 -0500

    Add files via upload

---
+++ BNB/MavrkTokenStandardBNB.sol
@@ -0,0 +1,54 @@
+
+// SPDX-License-Identifier: MIT
+// This contract was deployed at https://mavrk.xyz
+
+pragma solidity ^0.8.20;
+
+contract MavrkTokenStandard {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+    address public owner;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    constructor(string memory _name, string memory _symbol, address _deployer) {
+        name = _name;
+        symbol = _symbol;
+        decimals = 18;
+        totalSupply = 1_000_000_000 * (10 ** uint256(decimals));
+        owner = 0x000000000000000000000000000000000000dEaD; // renounced at launch
+        balanceOf[_deployer] = totalSupply;
+
+        emit Transfer(address(0), _deployer, totalSupply);
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool) {
+        allowance[msg.sender][spender] = amount;
+        emit Approval(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public returns (bool) {
+        require(balanceOf[from] >= amount, "Insufficient balance");
+        require(allowance[from][msg.sender] >= amount, "Insufficient allowance");
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount;
+        allowance[from][msg.sender] -= amount;
+        emit Transfer(from, to, amount);
+        return true;
+    }
+
+    function transfer(address to, uint256 amount) public returns (bool) {
+        require(balanceOf[msg.sender] >= amount, "Insufficient balance");
+        balanceOf[msg.sender] -= amount;
+        balanceOf[to] += amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
+    }
+}
